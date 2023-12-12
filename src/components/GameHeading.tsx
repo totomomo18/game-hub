bc9a6025c0840b9121c263ed2d10f1cd21bc1a99
TotoMomo <Totomomo18@gmail.com>
2023-12-12 02:33:13 +0200

    Add a dynamic page heading

---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,23 @@
+
+import { GameQuery } from '../App'
+import { Heading } from '@chakra-ui/react'
+
+interface Props{
+
+gameQuery:GameQuery
+
+}
+//${gameQuery.platform?.name} ${gameQuery.genre?.name}
+
+const GameHeading = ({gameQuery}:Props) => {
+
+  const heading=`${gameQuery.platform?.name || ''} ${gameQuery.genre?.name || ''} Games`
+
+  return (
+    <Heading as='h1' marginY={5} fontSize='5xl'>
+    {heading}    
+    </Heading>
+  )
+}
+
+export default GameHeading
